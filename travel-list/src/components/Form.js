65c4977f583987fc5760e885c200e4bb855140c4
Atul Dhotre <atuldhotre993@gmail.com>
2020-02-28 20:00:00 +0530

    added layout with react comp

---
+++ travel-list/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>what do yoy need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+export default Form;
---
+++ travel-list/src/components/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <h1>🏝️ Far Away 💼</h1>
+  )
+}
+
+export default Logo
