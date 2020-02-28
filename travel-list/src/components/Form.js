d22dfbc548f361164265754b4f31e462b9f7cbe7
Atul Dhotre <atuldhotre993@gmail.com>
2020-02-28 20:00:00 +0530

    added html code for from and some warning msg

--- travel-list/src/components/Form.js
+++ travel-list/src/components/Form.js
@@ -1,10 +1,24 @@
 import React from "react";
 
 function Form() {
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do yoy need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button type="submit">Add</button>
+    </form>
   );
 }
 
