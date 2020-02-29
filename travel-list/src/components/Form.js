d4b778e1dc210d7636c646a88649b6494ce9a75c
Atul Dhotre <atuldhotre993@gmail.com>
2020-02-29 20:00:00 +0530

    added react form controlled logic

--- travel-list/src/components/Form.js
+++ travel-list/src/components/Form.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
-  
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setDescription(value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log("newItem", newItem);
+
+    setDescription("");
+    setQuantity(1);
   };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do yoy need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(event) => setQuantity(Number(event.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={handleChange}
+      />
       <button type="submit">Add</button>
     </form>
   );
