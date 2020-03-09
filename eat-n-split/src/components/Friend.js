b0e2ae10a5b5459cb90379860e9fb68cc99281ca
Atul Dhotre <atuldhotre993@gmail.com>
2020-03-09 20:00:00 +0530

    on selecting friend changing btn text

--- eat-n-split/src/components/Friend.js
+++ eat-n-split/src/components/Friend.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Button from "./Button";
 
-function Friend({ friend, onSelection }) {
+function Friend({ friend, onSelection, selectedFriend }) {
+  const isSelected = selectedFriend?.id === friend.id
   return (
     <>
-      <li>
+      <li className={ isSelected ? "selected" : null}>
         <img src={friend.image} alt={friend.name} />
         <h3>{friend.name}</h3>
         {friend.balance < 0 && (<p className="red">You owe {friend.name} {Math.abs(friend.balance)} €</p>)}
         {friend.balance > 0 && (<p className="green"> {friend.name} owes you {Math.abs(friend.balance)} €</p>)}
         {friend.balance === 0 && (<p >You and {friend.name} are even</p>)}
-        <Button className="button" onClick={() => onSelection(friend)}>Select</Button>
+        <Button className="button" onClick={() => onSelection(friend)}>{isSelected ? "Close" : "Select"}</Button>
       </li>
     </>
   );
