795cbcd15c374b16917f0f41155e41055ab1d121
Atul Dhotre <atuldhotre993@gmail.com>
2020-03-13 20:00:00 +0530

    add layout logic

---
+++ usepopcorn/src/components/MainComp.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import ListBox from "./ListBox";
+import WatchedBox from "./WatchedBox";
+
+function Main({movies}) {
+  return (
+    <>
+      <main className="main">
+        <ListBox movies= {movies} />
+        <WatchedBox />
+      </main>
+    </>
+  );
+}
+
+export default Main;
